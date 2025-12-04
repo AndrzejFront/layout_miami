3ca2dbb823d968e3e6af453d43ce92d149c42f97
Andrzej Lipski <mustang12@gmail.com>
2025-12-04 08:17:31 +0100

    add task solution

--- src/main.js
+++ src/main.js
@@ -14,11 +14,3 @@ form.addEventListener("submit", function (event) {
   alert("Form submitted without reloading!");
 });
 
-
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
-  }
-});
